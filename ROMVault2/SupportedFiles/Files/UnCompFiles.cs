b50a162bb98180378f0393e6552a91e575bf03ed
edc71 <edc71@users.noreply.github.com>
2016-02-07 18:28:51 +0100

    Implemented double buffering on uncompressed files

--- ROMVault2/SupportedFiles/Files/UnCompFiles.cs
+++ ROMVault2/SupportedFiles/Files/UnCompFiles.cs
@@ -15,11 +15,14 @@ namespace ROMVault2.SupportedFiles.Files
     public static class UnCompFiles
     {
         private const int Buffersize = 4096*256*6;
-        private static readonly byte[] Buffer;
+        static byte[] Buffer;
+        static byte[] Buffer2;
 
         static UnCompFiles()
         {
             Buffer = new byte[Buffersize];
+            Buffer2 = new byte[Buffersize];
+
         }
 
         public static int CheckSumRead(string filename, bool testDeep, out byte[] crc, out byte[] bMD5, out byte[] bSHA1)
@@ -44,24 +47,44 @@ namespace ROMVault2.SupportedFiles.Files
 
                 long sizetogo = ds.Length;
 
+                int sizenow = sizetogo > Buffersize ? Buffersize : (int)sizetogo;
+                
+                ds.Read(Buffer, 0, sizenow);
+
+                Thread t2= null, t3 = null;
+                
                 while (sizetogo > 0)
                 {
-                    int sizenow = sizetogo > Buffersize ? Buffersize : (int)sizetogo;
 
-                    ds.Read(Buffer, 0, sizenow);
                     Thread t1 = new Thread(() => { crc32.TransformBlock(Buffer, 0, sizenow, null, 0); });
                     t1.Start();
                     if (testDeep)
                     {
-                        Thread t2 = new Thread(() => { md5.TransformBlock(Buffer, 0, sizenow, null, 0); });
-                        Thread t3 = new Thread(() => { sha1.TransformBlock(Buffer, 0, sizenow, null, 0); });
+                        t2 = new Thread(() => { md5.TransformBlock(Buffer, 0, sizenow, null, 0); });
+                        t3 = new Thread(() => { sha1.TransformBlock(Buffer, 0, sizenow, null, 0); });
                         t2.Start();
                         t3.Start();
+                    }
+
+                    Thread t4 = new Thread(() =>
+                    {
+                        sizetogo -= sizenow;
+                        sizenow = sizetogo > Buffersize ? Buffersize : (int)sizetogo;
+                        ds.Read(Buffer2, 0, sizenow);
+                    });
+
+                    t4.Start();
+
+                    if (testDeep)
+                    {
                         t2.Join();
                         t3.Join();
                     }
+                    t4.Join();
                     t1.Join();
-                    sizetogo -= sizenow;
+                    byte[] tmpbuffer = Buffer2;
+                    Buffer2 = Buffer;
+                    Buffer = tmpbuffer;
                 }
 
                 crc32.TransformFinalBlock(Buffer, 0, 0);
